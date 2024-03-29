d6b7b82ad5ed9d2c061b9e63852bc01a1f9d041d
sidsupar <siddharthamudgal99@gmail.com>
2024-03-29 22:16:48 +0530

    searchbar, pagination added

--- Blog_Web/frontend1/src/components/fullBlogSkel.tsx
+++ Blog_Web/frontend1/src/components/fullBlogSkel.tsx
@@ -155,7 +155,7 @@ export default function FullBlogSkel(){
                             
                         </div>
                     </div>                
- 
+                            {/* adasd */}
                     </div>
                 </div>
             
